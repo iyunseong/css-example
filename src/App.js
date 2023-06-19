b15157581d35910c95561031fa4425258b9af02b
이윤성 <120024332+iyunseong@users.noreply.github.com>
2023-06-19 13:30:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import TailwindComponent from "./TailwindComponent";
+import Button1 from "./components/Button1";
+import Button2 from "./components/Button2";
+import StyledComponent from "./styledComponent";
+
+function App() {
+  return (
+    <>
+      <Button1 />
+      <Button2 />
+      <StyledComponent />
+      <TailwindComponent />
+    </>
+  );
+}
+
+export default App;
